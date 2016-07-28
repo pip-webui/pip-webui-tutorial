6e9d386fc870dc41545bf98de791a5bf6ef90eed
dvoikinalex <94dvoikin@gmail.com>
2016-07-28 17:42:25 +0300

    update build.conf.js files and gulpfile.js files

--- step3/build.conf.js
+++ step3/build.conf.js
@@ -15,14 +15,5 @@ module.exports = {
         import: [
             'node_modules/pip-webui/dist/**/*'
         ]
-    },
-    samples: {
-        port: 8004,
-        publish: {
-            bucket: 'webui.pipdevs.com',
-            accessKeyId: 'AKIAIEXTTAEEHYPHS3OQ',
-            secretAccessKey: 'otMg2vQLZjF4Nkb90j1prtugoUCNm3XqLS/KkHyc',
-            region: 'us-west-1'
-        }
     }
 };
--- step4/gulpfile.js
+++ step4/gulpfile.js
@@ -8,9 +8,6 @@ gulp.task('build', ['build-dev', 'build-prod']);
 gulp.task('rebuild', ['build-dev']);
 gulp.task('clean', ['build-clean']);
 gulp.task('watch', ['build-watch']);
-gulp.task('jshint', ['test-jshint']);
-gulp.task('launch', ['samples-launch']);
-gulp.task('publish', ['samples-publish']);
 
 // Set default task
 gulp.task('default', ['build']);
